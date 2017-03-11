7f811a584e5042e76120f368e7dd7a4b97ea22e3
Johanni Brea <johannibrea@gmail.com>
2017-03-11 14:19:18 +0100

    handle input with argparse

--- experiments/learningratedependence.jl
+++ experiments/learningratedependence.jl
@@ -1,6 +1,34 @@
 include("../equilibriumprop.jl")
 include("../backprop.jl")
 using Distributions
+using ArgParse
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--scaling", "-s"
+        help = "constant or linear"
+		arg_type = AbstractString
+		default = "constant"
+    "--nhidden", "-n"
+		help = "number of hidden layers (1 or 2)"
+        arg_type = Int
+        default = 1
+	"--stepsforward", "-f"
+		help = "number of steps in forward phase"
+		arg_type = Int
+		default = 50
+    "--withbackprop", "-b"
+        help = "run also backprop"
+        action = :store_true
+	"--nofsamples", "-t"
+		help = "number of samples"
+		arg_type = Int
+		default = 5*10^6
+    "id"
+        help = "simulation id"
+        required = true
+end
+pa = parse_args(s)
 
 function shuffle(x)
 	sample(x[:], size(x), replace = false)
@@ -9,17 +37,17 @@ function linearScaleT0(T0, i, range)
 	T0*i*range
 end
 
-if ARGS[2] == "L"
+if pa["scaling"] == "linear"
 	println("linear scaling")
 	scaleT0 = linearScaleT0
 else
 	scaleT0(T0, i, range) = T0
 end
 
-T0 = 5*10^6
+T0 = pa["nofsamples"]
 lr0 = 10.
-ns = [2; 30; 2]
-stepsforward = 50
+ns = pa["nhidden"] == 1 ? [2; 30; 2] : [2; 30; 30; 2]
+stepsforward = pa["stepsforward"]
 
 lrs = []
 lossesbp = []
@@ -27,18 +55,20 @@ lossesstatich = []
 losseseq = []
 for range in [1]
 	for i in 2.^collect(0:5)
-		for j in 1:5
-			net = BackpropNetwork(ns)
-			conf = BackpropConfig(net, 
-								  learningratefactor = lr0/i/range/4, 
-								  n_ofsamples = scaleT0(T0, i, range))
-			push!(lossesbp, learn!(net, conf))
-			push!(lrs, lr0/i/range)
-			net2 = BackpropNetwork(ns)
-			net2.b[1] = shuffle(net.b[1])
-			net2.w[1] = shuffle(net.w[1])
-			conf.learningrate[1] = 0;
-			push!(lossesstatich, learn!(net2, conf))
+		for j in 1:2
+			if pa["withbackprop"]
+				net = BackpropNetwork(ns)
+				conf = BackpropConfig(net, 
+									  learningratefactor = lr0/i/range/4, 
+									  n_ofsamples = scaleT0(T0, i, range))
+				push!(lossesbp, learn!(net, conf))
+				push!(lrs, lr0/i/range)
+				net2 = BackpropNetwork(ns)
+				net2.b[1] = shuffle(net.b[1])
+				net2.w[1] = shuffle(net.w[1])
+				conf.learningrate[1] = 0;
+				push!(lossesstatich, learn!(net2, conf))
+			end
 			net = getequipropnet(ns)
 			conf = EquipropConfig(net,
 								  stepsforward = stepsforward,
@@ -52,6 +82,10 @@ end
 
 using JLD
 run(`mkdir -p $datapath/learningratedependence`)
-@save "$datapath/learningratedependence/backprop$(ARGS[2])-$(ARGS[1]).jld" lrs lossesbp
-@save "$datapath/learningratedependence/statich$(ARGS[2])-$(ARGS[1]).jld" lrs lossesstatich
-@save "$datapath/learningratedependence/equiprop$(ARGS[2])-$(ARGS[1]).jld" lrs losseseq
+fileid = "-$(pa["id"])-$(pa["nhidden"])-$(pa["nofsamples"])-$(pa["scaling"])"
+if pa["withbackprop"]
+	@save "$datapath/learningratedependence/backprop$fileid.jld" lrs lossesbp
+	@save "$datapath/learningratedependence/statich$fileid.jld" lrs lossesstatich
+end
+fileid *= "-$(pa["stepsforward"])"
+@save "$datapath/learningratedependence/equiprop$fileid.jld" lrs losseseq
