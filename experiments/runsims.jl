7f8078cadca13637484977e4abe55d442022b3c7
Johanni Brea <johannibrea@gmail.com>
2017-03-29 14:54:19 +0200

    eval parsed arguments

--- experiments/runsims.jl
+++ experiments/runsims.jl
@@ -4,9 +4,9 @@ ns = [2; 30; 2]
 for arg in ARGS
 	eq = search(arg, '=')
 	if arg[1:eq-1] == "ns"
-		ns = parse(arg[eq+1:end])
+		ns = eval(parse(arg[eq+1:end]))
 	else
-		push!(vargs, (Symbol(arg[1:eq-1]), parse(arg[eq+1:end])))
+		push!(vargs, (Symbol(arg[1:eq-1]), eval(parse(arg[eq+1:end]))))
 	end
 end
 createandrunsim(ns; vargs...)
