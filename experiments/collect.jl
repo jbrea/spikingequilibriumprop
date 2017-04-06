7abe161e8a3456be22920a0ad9e7ab9fa04b9b23
Johanni Brea <johannibrea@gmail.com>
2017-04-06 17:02:34 +0200

    add fields and don't reload already imported sims

--- experiments/collect.jl
+++ experiments/collect.jl
@@ -7,14 +7,23 @@ data = DataFrame(filename = AbstractString[],
 				 losses = Array{Float64, 1}[], 
 				 endloss = Float64[], 
 				 beta = Float64[], 
-				 learningratefactor = Float64[])
+				 learningratefactor = Float64[],
+				 noiselevel = Float64[],
+				 nofhidden = Float64[])
 
 datadir = SpikingEquilibriumProp.datapath * "/dump"
+summarypath = SpikingEquilibriumProp.datapath * "/summary.jld"
+data = isfile(summarypath) ? load(summarypath, "data") : data
+loadedfiles = data[:filename]
 for file in readdir(datadir)
-	try
-	net, conf, losses = load("$datadir/$file", "net", "conf", "losses")
-	push!(data, [file, conf.stepsforward, conf.stepsbackward, conf.n_ofsamples,
-			  losses, losses[end], conf.beta, conf.learningratefactor])
+	if file in loadedfiles
+		nothing
+	else
+		net, conf, losses = load("$datadir/$file", "net", "conf", "losses")
+		push!(data, [file, conf.stepsforward, conf.stepsbackward, conf.n_ofsamples,
+			  losses, losses[end], conf.beta, conf.learningratefactor,
+			  net.layers[:hiddenlayer1].neurons.p.noiselevel, 
+			  length(net.layers) - 5])
 	end
 end
-@save SpikingEquilibriumProp.datapath * "/summary.jld" data
+@save summarypath data
