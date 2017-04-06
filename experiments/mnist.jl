1559094a4dfa362b50c4bb8e3cd960cd593a5d49
Johanni Brea <johannibrea@gmail.com>
2017-04-06 17:00:12 +0200

    adapt to module, simplify

--- experiments/mnist.jl
+++ experiments/mnist.jl
@@ -1,23 +1,30 @@
-include("../equilibriumprop.jl")
+using SpikingEquilibriumProp, JLD
 include("../mnistfunctions.jl")
-BLAS.set_num_threads(4)
+BLAS.set_num_threads(2)
 
-net = getequipropnet([28^2; 500; 10], targetdimension = 10);
-conf = EquipropConfig(net, stepsforward = 50, 
-						   stepsbackward = 4,
-						   learningratefactor = 1., 
-						   inputfunction = getimg, 
-						   targetfunction = getlabel, 
-						   n_ofsamples = 10^5, records = 10,
-						   outputprocessor = getoutputprediction);
+net, conf = getequipropnetandconf([28^2; 500; 500; 10],
+								  targetdimension = 10,
+								  stepsforward = 100, 
+								  stepsbackward = 1,
+								  learningratefactor = .005, 
+								  inputfunction = getimg, 
+								  targetfunction = getlabel, 
+								  n_ofsamples = 10^4, records = 10,
+								  outputprocessor = 
+								  SpikingEquilibriumProp.getoutputprediction,
+								  beta = 2,
+								  backwardupdate =
+								  SpikingEquilibriumProp.NetSim.updatenetasync!);
 
 l = Float64[]
 traininge = Float64[]
-teste = Float64[]
+vales = Float64[]
 for _ in 1:10
 	l = [l; learn!(net, conf)]
-	push!(traininge, trainingerror(net, conf))
-	push!(teste, testerror(net, conf))
+	#push!(traininge, trainingerror(net, conf))
+	vale = valerror(net, conf)
+	println(vale)
+	push!(vales, vale)
 end
-run(`mkdir -p $datapath/mnist`)
-@save "$datapath/mnist/mnistratenet.jld" net conf l traininge teste
+run(`mkdir -p $(SpikingEquilibriumProp.datapath)/mnist`)
+@save "$(SpikingEquilibriumProp.datapath)/mnist/mnistratenet2.jld" net conf l traininge vales
--- mnistfunctions.jl
+++ mnistfunctions.jl
@@ -1,9 +1,11 @@
 using MNIST
-imgs, labels = traindata()
+imgstrain, labelstrain = traindata()
+imgs, labels = imgstrain[:, 1:50000], labelstrain[1:50000]
+imgsval, labelsval = imgstrain[:, 50001:end], labelstrain[50001:end]
 imgstest, labelstest = testdata()
 
 function getimg()
-	global patternindex = rand(1:60000)
+	global patternindex = rand(1:50000)
 	imgs[:, patternindex]
 end
 
@@ -14,12 +16,15 @@ end
 function geterror(net, conf, imgs, labels)
 	error = 0
 	for i in 1:length(labels)
-		error += indmax(forwardphase!(net, conf, imgs[:, i])) != labels[i] + 1
+		error += indmax(SpikingEquilibriumProp.forwardphase!(net, conf, imgs[:, i])) != labels[i] + 1
 	end
 	error/length(labels)
 end
 function trainingerror(net, conf)
 	geterror(net, conf, imgs, labels)
+end
+function valerror(net, conf)
+	geterror(net, conf, imgsval, labelsval)
 end
 function testerror(net, conf)
 	geterror(net, conf, imgstest, labelstest)
